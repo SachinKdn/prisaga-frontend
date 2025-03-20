6694ce4844707df5026ca823c21a681bc7492021
Sachin Kadian <sachinkadian2019@gmail.com>
2025-03-20 10:10:29 +0530

    Company logo changed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,19 @@
 import { Box } from '@mui/material';
-import LogoImage from '@assets/png/PrisagaHiring+-png.png';
-import Image from 'next/image';
+import Logo from '@assets/svg/prisaga-svg-logo.svg';
 import LoginForm from '@components/LoginForm';
 
 export default function Page() {
   return (
     <Box sx={styles.wrapper}>
       <Box sx={styles.header}>
-        <Image
+        <Logo />
+        {/* <Image
           src={LogoImage}
           alt="Company Logo"
           width={200}
           // height={100}
           style={styles.logo}
-        />
+        /> */}
       </Box>
       <LoginForm />
     </Box>
@@ -29,6 +29,7 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     width: '100%',
+    margin: '15px',
   },
   logo: {
     // width: 'auto',
